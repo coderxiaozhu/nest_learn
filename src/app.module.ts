c4847218fac7d5dfc0cb312534bf137be0cc6f16
coderxiaozhu <861353958@qq.com>
2023-07-08 13:11:38 +0800

    提交备份

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm'
+import * as Joi from 'joi'
+import * as dotenv from 'dotenv'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
-import * as Joi from 'joi'
+import { LogsModule } from './logs/logs.module';
+import { RolesModule } from './roles/roles.module';
+import { User } from './user/entities/user.entity'
+import { configEnum } from './enum/config.enum';
 
 const schema = Joi.object({
   DB_PORT: Joi.number().default(3306),
@@ -14,13 +20,34 @@ const schema = Joi.object({
   DB_HOST: Joi.alternatives().try(Joi.string().ip(), Joi.string().domain())
 })
 
+const envFilePath = '.env';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: schema
+      validationSchema: schema,
+      envFilePath,
+      load: [() => dotenv.config({ path: '.env' })]
     }),
-    UserModule
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get(configEnum.DB_TYPE),
+        host: configService.get(configEnum.DB_HOST),
+        port: configService.get(configEnum.DB_PORT),
+        username: configService.get(configEnum.DB_USERNAME),
+        password: configService.get(configEnum.DB_PASSWORD),
+        database: configService.get(configEnum.DB_DATABASE),
+        entities: [User],
+        // autoLoadEntities: true,
+        synchronize: configService.get(configEnum.DB_SYNC)
+      } as TypeOrmModuleOptions)
+    }),
+    UserModule,
+    LogsModule,
+    RolesModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,22 @@
-export class User {}
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, ManyToMany, JoinTable } from 'typeorm'
+import { Logs } from 'src/logs/entities/log.entity';
+import { Roles } from 'src/roles/entities/role.entity';
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+    
+    @Column()
+    username: string;
+
+    @Column() 
+    password: string;
+
+    // @OneToMany(() => Logs, (logs) => logs.user)
+    // logs: Logs[];
+
+    // @ManyToMany(() => Roles, (role) => role.users)
+    // @JoinTable({ name: "users_roles" })
+    // roles: Roles[]
+}
